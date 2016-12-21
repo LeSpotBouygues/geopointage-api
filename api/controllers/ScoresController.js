b1edc99975b9c20c57ad1d32c6ae983926f6e0bf
joset_s <samuel.joset@epitech.eu>
2016-12-21 01:47:54 +0100

    fix import

--- api/controllers/ScoresController.js
+++ api/controllers/ScoresController.js
@@ -75,37 +75,32 @@ router.post('/import', urlencodedParser, function(req, res, next) {
     
     var scores = [];
 
-    // var body = "[{login: 'id001', address: '1 rue blabla', date:'10-12-2016', numberOfHours: '2', worker: { firstName:'Samuel', lastName: 'JOSET'} }, {login: 'id002', address: '6 rue ha', date:'2-10-2006', numberOfHours: '4', worker: { firstName:'David', lastName: 'Haga'} }]";
+    // var body2 = "[{login: 'id001', address: '1 rue blabla', date:'10-12-2016', numberOfHours: '2', worker: { firstName:'Samuel', lastName: 'JOSET'} }, {login: 'id002', address: '6 rue ha', date:'2-10-2006', numberOfHours: '4', worker: { firstName:'David', lastName: 'Haga'} }]";
     
-    var body = JSON.stringify(eval("(" + req.body.body + ")"));
-    
-    body = JSON.parse(body);
+    var body = JSON.parse(req.body.body);
 
     for (var i = 0; i < body.length; i++) {
-
-	var st = body[i].date;
-	var pattern = /(\d{2})\-(\d{2})\-(\d{4})/;
-	var dt = new Date(st.replace(pattern,'$3-$2-$1'));
-
-	var score = {
-	    login: body[i].login,
-	    address: body[i].address,
-	    date: dt,
-	    numberOfHours: body[i].numberOfHours,
-	    worker: {
-		firstName: body[i].worker.firstName,
-		lastName: body[i].worker.lastName
-	    }
-	};
-
-	scores.push(score);
-    	// console.log("lastName = " + obj[i][8] + " ;firstName = " + obj[i][9] + " ;identifiant = " + obj[i][1]);
+	for (var j = 0; j < body[i].workers.length; j++) {
+	    var st = body[i].date;
+	    var pattern = /(\d{2})\-(\d{2})\-(\d{4})/;
+	    var dt = new Date(st.replace(pattern,'$3-$2-$1'));
+	    
+	    var score = {
+		login: body[i].login,
+		address: body[i].address,
+		date: dt,
+		numberOfHours: body[i].numberOfHours,
+		worker: {
+		    firstName: body[i].workers[j].firstName,
+		    lastName: body[i].workers[j].lastName
+		}
+	    };
+	    scores.push(score);
+	}
     }
 
-    // console.log(scores);
     Score.collection.insert(scores, function(err) {
 	if (err) {
-	    // console.log(err);
 	    return res.jsend.fail(err);
 	}
 	res.status(201).jsend.success({ message: 'Scores created!' });
